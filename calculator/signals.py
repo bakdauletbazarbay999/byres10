cedb9563a5da4e7816953dd0ac2daf351299f58d
bakdauletbazarbay999 <bakdauletbazarbay9@gmail.com>
2026-04-22 00:35:08 +0500

    Add files via upload

---
+++ calculator/signals.py
@@ -0,0 +1,14 @@
+# In your app's signals.py
+
+from django.db.models.signals import post_save
+from django.contrib.auth import get_user_model
+from django.dispatch import receiver
+from .models import Profile # Adjust import to your actual Profile model location
+
+User = get_user_model()
+
+@receiver(post_save, sender=User)
+def create_user_profile(sender, instance, created, **kwargs):
+    # 'created' is a boolean flag indicating if the User was just created
+    if created:
+        Profile.objects.create(user=instance)
