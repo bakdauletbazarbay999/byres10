fa5892a3c6cc023685497c8127c41b14434bb8eb
bakdauletbazarbay999 <bakdauletbazarbay9@gmail.com>
2026-04-22 16:01:17 +0500

    Delete calculator directory

--- calculator/apps.py
+++
@@ -1,9 +0,0 @@
-# In your app's apps.py
-
-from django.apps import AppConfig
-
-class YourAppConfig(AppConfig):
-    name = 'calculator'
-
-    def ready(self):
-        import calculator.signals # Ensure this path is correct
